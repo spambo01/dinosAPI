aa8ac7e6a256df0db40da0b9063eb41e9de2c898
savvas pamboukas <savvaspamboukas@hotmail.com>
2018-08-07 10:03:57 +0300

    initialize

---
+++ DinoAPI/DinoAPI/Models/Trade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinoAPI.Models
+{
+    public class Trade
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Product { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string Currency { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ DinoAPI/DinoAPI/Models/TradeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DinoAPI.Models
+{
+    public class TradeContext:DbContext
+    {
+        public TradeContext(DbContextOptions<TradeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trade> TodoItems { get; set; }
+    }
+}
